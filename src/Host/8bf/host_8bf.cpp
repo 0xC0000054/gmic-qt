83fad6f71217ab2988dee041256b75e238886ec9
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2021-06-08 17:43:19 -0600

    Only show the progress dialog when running with the last parameters

--- src/Host/8bf/host_8bf.cpp
+++ src/Host/8bf/host_8bf.cpp
@@ -1827,7 +1827,7 @@ int main(int argc, char *argv[])
     {
         GmicQt::RunParameters parameters;
         parameters = GmicQt::lastAppliedFilterRunParameters(GmicQt::ReturnedRunParametersFlag::AfterFilterExecution);
-        exitCode = GmicQt::run(GmicQt::UserInterfaceMode::Full,
+        exitCode = GmicQt::run(GmicQt::UserInterfaceMode::ProgressDialog,
                                parameters,
                                disabledInputModes,
                                disabledOutputModes,
